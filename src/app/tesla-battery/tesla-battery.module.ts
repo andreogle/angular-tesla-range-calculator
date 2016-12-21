98341485bfae6ac323d7fc55f2f7c3f6f5ee3715
Andre Ogle <ogleandre@yahoo.com>
2016-12-21 17:44:03 +0200

    Work on stats component

--- src/app/tesla-battery/tesla-battery.module.ts
+++ src/app/tesla-battery/tesla-battery.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { TeslaBatteryComponent } from './containers/tesla-battery.component';
+import { TeslaCarComponent } from './components/tesla-car/tesla-car.component';
+import { TeslaStatsComponent } from './components/tesla-stats/tesla-stats.component';
 
 import { BatteryService } from './tesla-battery.service';
 
 @NgModule({
   declarations: [
-    TeslaBatteryComponent
+    TeslaBatteryComponent,
+    TeslaCarComponent,
+    TeslaStatsComponent,
   ],
   imports: [
     CommonModule,
